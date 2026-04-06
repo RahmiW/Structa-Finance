068e2752779ea87b2201ec732f458f5c2888a4f7
RahmiW <abdulrahmanwardere@gmail.com>
2026-04-06 01:18:55 -0500

    Added default account type method in AccountService

---
+++ src/main/java/com/structa/bank/services/AccountService.java
@@ -0,0 +1,35 @@
+package com.structa.bank.services;
+
+import com.structa.bank.entities.Account;
+import com.structa.bank.entities.User;
+import com.structa.bank.repositories.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public void createDefaultAccounts(User user) {
+        // Checking default
+        Account checking = new Account();
+        checking.setName("Checking");
+        checking.setBalance(BigDecimal.ZERO);
+        checking.setDefault(true);
+        checking.setUser(user);
+
+        // Savings default
+        Account savings = new Account();
+        savings.setName("Savings");
+        savings.setBalance(BigDecimal.ZERO);
+        savings.setDefault(true);
+        savings.setUser(user);
+
+        // Save both
+        accountRepository.save(checking);
+        accountRepository.save(savings);
+    }
+}
