b29318ba61c51ee617c9bc65c909e75b6691e2e1
RahmiW <abdulrahmanwardere@gmail.com>
2026-04-06 00:35:44 -0500

    Add to account entity

--- src/main/java/com/structa/bank/entities/Account.java
+++ src/main/java/com/structa/bank/entities/Account.java
@@ -1,4 +1,25 @@
 package com.structa.bank.entities;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    private boolean isDefault;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
